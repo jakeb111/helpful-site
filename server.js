894493e531a91fd671a7e956aa7069de1cc95868
Jakeb Barnett <jakeb2barnett@gmail.com>
2018-12-04 11:27:27 -0500

    Boilerplate

---
+++ server.js
@@ -0,0 +1,24 @@
+var bodyParser = require('body-parser');
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+
+app.use(express.static('views'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.json());
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res){
+    res.render('pages/index');
+});
+
+app.get('*', function(req, res) {
+    res.render('pages/404');
+});
+
+http.listen(80, function(){
+    console.log('listening on localhost');
+});
